9754d0f76d4416699a159b2f71b6615d0498a8a2
Naveen Reddy D <dnaveen356@gmail.com>
2022-09-03 12:25:39 +0530

    2 decoders - one for faces,captions and other to get the characters.

--- models/face_caption_model.py
+++ models/face_caption_model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from .modules.sent_embedding import SentEmbedding
 
 import ipdb
 
@@ -8,11 +9,23 @@ class FaceCaptionModel(nn.Module):
     def __init__(self, opt):
         super(FaceCaptionModel, self).__init__()
         self.memory_encoding_size = opt.encoding_size
+        self.use_bert_embedding = 'use_bert_embedding' in vars(opt) and opt.use_bert_embedding
         self.drop_prob_lm = opt.drop_prob_lm
         self.dropout = nn.Dropout(self.drop_prob_lm)
         self.classifier_type = opt.classifier_type
         self.transformer = torch.nn.Transformer(self.memory_encoding_size)
 
+        # Sentence Embedding
+         # sent embed
+        self.rnn_size = opt.rnn_size
+        self.sent_embed = SentEmbedding(opt)
+
+        # Bert Embedding 
+        if self.use_bert_embedding:
+            self.bert_embedding = nn.Linear(opt.bert_size, self.rnn_size)
+            print('===Use Bert Embedding===', self.use_bert_embedding)
+
+        # Face Embedding
         self.face_encoding_size = opt.face_encoding_size
         self.face_feat_size = opt.face_feat_size - 6
         self.face_embed = nn.Linear(self.face_feat_size, self.face_encoding_size)
@@ -38,12 +51,28 @@ class FaceCaptionModel(nn.Module):
         return face_features
 
 
+    def _get_required_caption_features(self, slot_size, captions, caption_masks):
+        label_features = None
+        for slot in range(slot_size):
+            label_feat = self.sent_embed(captions[:,slot], caption_masks[:,slot])
+            if slot > 0:
+                label_features = torch.cat((label_features, label_feat),dim=1)
+            else:
+                label_features = label_feat
+        
+        label_features = label_features.reshape(64, slot_size, 512)
+
+        return label_features
+
+
+
     def forward(self, fc_feats, face_feats, face_masks, captions, caption_masks, slots, slot_masks, slot_size,
                 characters, genders=None):
         # get memory of features for each slot
         ipdb.set_trace()
         characters = characters[:,:slot_size+1]
         face_features = self._get_required_face_features(slot_size, face_feats)
+        caption_features  = self._get_required_caption_features(slot_size, captions, caption_masks)
         character_embed = self.character_embed(characters)
         masks = slot_masks[:,:slot_size+1].bool()
 
@@ -52,11 +81,12 @@ class FaceCaptionModel(nn.Module):
             src_mask = self.transformer.generate_square_subsequent_mask(masks.size(1)-1).cuda()
             tgt_mask = self.transformer.generate_square_subsequent_mask(masks.size(1)).cuda()
             ipdb.set_trace()
-            decoder_output = self.transformer_decoder(character_embed.transpose(0,1), face_features, 
+            cap_face_decoder_output = self.transformer_decoder(caption_features.transpose(0,1), face_features.transpose(0,1))
+            decoder_output = self.transformer_decoder(character_embed.transpose(0,1), cap_face_decoder_output, 
                                                       tgt_mask=tgt_mask,
                                                       tgt_key_padding_mask=transformer_masks)
-            logits = self.logit(decoder_output)
-            logprobs = F.log_softmax(logits[:,-1,:],dim=2)
+            logits = self.logit(self.dropout(decoder_output.transpose(0, 1)[:,:-1]))
+            logprobs = F.log_softmax(logits, dim=2)
 
 
         loss = self.loss(logprobs, characters[:,1:], masks[:,1:].float())
